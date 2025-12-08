18e71e7fc67ff065d37508b0a568a38fa05b965c
yizhenyi <yizhenyi@pudutche.com>
2025-12-08 19:12:24 +0800

    feat: 支持环境变量写入

--- packages/client/src/views/home/index.tsx
+++ packages/client/src/views/home/index.tsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
+  const apiUrl = import.meta.env.VITE_API_URL;
   return (
     <div>
       <h1>Home</h1>
+      <p>{apiUrl}</p>
     </div>
   );
 }
--- packages/service/src/index.ts
+++ packages/service/src/index.ts
@@ -1,8 +1,8 @@
-import app from './app';
+import "dotenv/config";
+import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
-
